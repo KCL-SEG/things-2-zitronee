96e7f49097f02d2ba97c4edab1f37b7007e9e18d
Ryan Yan <ryan.yan@kcl.ac.uk>
2022-10-19 12:45:30 +0100

    First attempt
    Added the form with model form.

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,11 @@
 """Forms of the project."""
 
 # Create your forms here.
+from django import forms
+from .models import Thing
+
+class ThingForm(forms.ModelForm):
+    class Meta:
+        model = Thing
+        fields = ['name','description','quantity']
+        widgets = { 'description': forms.Textarea(), 'quantity':forms.NumberInput() }
--- things/views.py
+++ things/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
-
+from .forms import ThingForm
 def home(request):
+    form = ThingForm()
     return render(request, 'home.html', {'form': form})
